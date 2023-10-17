46f77e9299927ec78150a9605fc43936880ddb9f
Talha Husnain <24100245@lums.edu.pk>
2023-10-17 12:29:32 -0700

    updated

--- Events.java
+++ Events.java
@@ -1,61 +1,125 @@
 /**
- * This class represents an event in the simulation.
- * Each event has an ID, a type, and a time of occurrence.
- * 
- * @author YourName
+ * Represents an event occurring in the modem bank simulation.
+ * An event can be of different types such as "DIAL_IN" or "HANG_UP".
+ * Each event has attributes like ID, type, occurrence time, and the time it
+ * entered the system.
+ * The class provides getters and setters for each attribute and implements
+ * the Comparable interface to enable prioritizing events based on their
+ * occurrence times.
  */
 public class Event implements Comparable<Event> {
-    private int id;  // Event ID
-    private String type;  // Type of the event ("Dial-In" or "Hang-Up")
-    private int time;  // Time of occurrence
 
-    /**
-     * Constructs a new Event.
-     * 
-     * @param id   The ID of the event.
-     * @param type The type of the event.
-     * @param time The time of occurrence.
-     */
-    public Event(int id, String type, int time) {
-        this.id = id;
-        this.type = type;
-        this.time = time;
-    }
+  // Unique identifier for the event to distinguish between multiple events.
+  private int id;
 
-    /**
-     * Compares this event with another event based on their time of occurrence.
-     * 
-     * @param other The other event.
-     * @return A negative, zero, or positive integer based on the comparison.
-     */
-    @Override
-    public int compareTo(Event other) {
-        return Integer.compare(this.time, other.time);
-    }
+  // Type of event, such as "DIAL_IN" or "HANG_UP", indicating the nature of the
+  // event.
+  private String type;
 
-    // Getter methods
-    public int getId() {
-        return id;
-    }
+  // The specific time unit when this event is set to happen.
+  private int time;
 
-    public String getType() {
-        return type;
-    }
+  // The time unit when this event was introduced into the system. Useful for
+  // calculating wait times.
+  private int entryTime;
 
-    public int getTime() {
-        return time;
-    }
+  /**
+   * Constructs a new Event with the given attributes.
+   *
+   * @param id        Unique identifier assigned to the event.
+   * @param type      Describes the nature of the event - e.g., "DIAL_IN" means a
+   *                  user is trying to connect.
+   * @param time      Indicates when the event will occur in the simulation.
+   * @param entryTime Specifies when the event was introduced into the system.
+   */
+  public Event(int id, String type, int time, int entryTime) {
+    this.id = id;
+    this.type = type;
+    this.time = time;
+    this.entryTime = entryTime;
+  }
 
-    // Setter methods
-    public void setId(int id) {
-        this.id = id;
-    }
+  /**
+   * Provides natural ordering for the Event class based on the event's occurrence
+   * time.
+   * This is essential for prioritizing events in the simulation timeline.
+   *
+   * @param other Another event to compare this event against.
+   * @return Negative value if this event is before the other, positive if after,
+   *         and zero if they occur at the same time.
+   */
+  @Override
+  public int compareTo(Event other) {
+    return Integer.compare(this.time, other.time);
+  }
 
-    public void setType(String type) {
-        this.type = type;
-    }
+  // Getter methods:
 
-    public void setTime(int time) {
-        this.time = time;
-    }
+  /**
+   * @return The unique identifier associated with this event.
+   */
+  public int getId() {
+    return id;
+  }
+
+  /**
+   * @return The type of this event, indicating its nature.
+   */
+  public String getType() {
+    return type;
+  }
+
+  /**
+   * @return The specific simulation time unit when this event will occur.
+   */
+  public int getTime() {
+    return time;
+  }
+
+  /**
+   * @return The simulation time unit when this event was first introduced into
+   *         the system.
+   */
+  public int getEntryTime() {
+    return entryTime;
+  }
+
+  // Setter methods:
+
+  /**
+   * Update the ID of this event.
+   *
+   * @param id The new identifier to be assigned to this event.
+   */
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  /**
+   * Change the type of this event.
+   *
+   * @param type The new type describing the nature of this event.
+   */
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  /**
+   * Modify the time of occurrence for this event.
+   *
+   * @param time The new simulation time unit when this event should occur.
+   */
+  public void setTime(int time) {
+    this.time = time;
+  }
+
+  /**
+   * Update when this event was first introduced into the system.
+   *
+   * @param entryTime The new simulation time unit marking this event's
+   *                  introduction.
+   */
+  public void setEntryTime(int entryTime) {
+    this.entryTime = entryTime;
+  }
 }
--- ModemBankSimulation.java
+++ ModemBankSimulation.java
@@ -1,112 +1,153 @@
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.File;
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * This class simulates a modem bank, where users dial in and either get a modem or are placed in a waiting queue.
+ * If all modems are occupied, the user request can either be placed in a queue (if not full) or dropped.
+ */
 public class ModemBankSimulation {
-  private PriorityQueue<Event> eventQueue;
-  private RegularQueue<Integer> waitingQueue;
-  private int numModems;
-  private int queueCapacity;
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    String anotherSimulation;
 
-    do {
-      System.out.println("Enter the length of the simulation:");
-      int lengthOfSimulation = scanner.nextInt();
+    // Event queue to handle user dial-in and hang-up events.
+    private PriorityQueue<Event> eventQueue;
 
-      System.out.println("Enter the average time between dial-in attempts:");
-      double averageDialInTime = scanner.nextDouble();
+    // Queue to handle users who are waiting for a modem.
+    private RegularQueue<Integer> waitingQueue;
 
-      System.out.println("Enter the average connection time:");
-      int averageConnectionTime = scanner.nextInt();
+    // Total number of modems.
+    private int numModems;
 
-      System.out.println("Enter the number of modems in the bank:");
-      int numModems = scanner.nextInt();
+    // Maximum size of the waiting queue. -1 indicates an infinite queue.
+    private int queueCapacity;
 
-      System.out.println("Enter the size of the waiting queue or -1 for an infinite queue:");
-      int queueCapacity = scanner.nextInt();
+    // Used to generate unique IDs for users.
+    private int nextUserId = 1;
 
-      ModemBankSimulation simulation = new ModemBankSimulation();
-      simulation.runSimulation(lengthOfSimulation, averageDialInTime, averageConnectionTime, numModems, queueCapacity);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String anotherSimulation;
 
-      System.out.println("Do you want to run another simulation? (yes/no)");
-      anotherSimulation = scanner.next();
+        // Loop to keep running simulations until the user decides to stop.
+        do {
+            // Collecting parameters for the simulation from the user.
+            System.out.println("Enter the length of the simulation:");
+            int lengthOfSimulation = scanner.nextInt();
 
-    } while (anotherSimulation.equalsIgnoreCase("yes"));
+            System.out.println("Enter the average time between dial-in attempts:");
+            double averageDialInTime = scanner.nextDouble();
 
-    scanner.close();
-  }
+            System.out.println("Enter the average connection time:");
+            int averageConnectionTime = scanner.nextInt();
 
-  public void runSimulation(int lengthOfSimulation, double averageDialInTime, int averageConnectionTime,
-      int numModems, int queueCapacity) {
-    this.eventQueue = new PriorityQueue<>();
-    this.waitingQueue = new RegularQueue<>(queueCapacity == -1 ? 100 : queueCapacity, queueCapacity == -1); // Start with a reasonable size if infinite
-    this.numModems = numModems;
-    this.queueCapacity = queueCapacity;
+            System.out.println("Enter the number of modems in the bank:");
+            int numModems = scanner.nextInt();
 
-    Random rand = new Random();
-    for (int i = 0; i < lengthOfSimulation; i++) {
-      int numDialInEvents = (int) (rand.nextFloat() < averageDialInTime ? 1 : 0);
-      for (int j = 0; j < numDialInEvents; j++) {
-        eventQueue.offer(new Event(i, "DIAL_IN", i));
-      }
+            System.out.println("Enter the size of the waiting queue or -1 for an infinite queue:");
+            int queueCapacity = scanner.nextInt();
+
+            // Running the simulation with the provided parameters.
+            ModemBankSimulation simulation = new ModemBankSimulation();
+            simulation.runSimulation(lengthOfSimulation, averageDialInTime, averageConnectionTime, numModems, queueCapacity);
+
+            System.out.println("Do you want to run another simulation? (yes/no)");
+            anotherSimulation = scanner.next();
+
+        } while (anotherSimulation.equalsIgnoreCase("yes"));
+
+        scanner.close();
     }
 
-    int currentTime = 0;
-    int totalWaitTime = 0;
-    int totalModemBusyTime = 0;
-    int totalUsers = 0;
+    /**
+     * This method runs a simulation based on the provided parameters.
+     */
+    public void runSimulation(int lengthOfSimulation, double averageDialInTime, int averageConnectionTime,
+                              int numModems, int queueCapacity) {
+        // Initializing event and waiting queues.
+        this.eventQueue = new PriorityQueue<>();
+        this.waitingQueue = new RegularQueue<>(queueCapacity == -1 ? 100 : queueCapacity, queueCapacity == -1);
+        this.numModems = numModems;
+        this.queueCapacity = queueCapacity;
+        this.nextUserId = 1;
 
-    while (currentTime < lengthOfSimulation) {
-      Event event = eventQueue.poll();
-      while (event != null && event.getTime() == currentTime) {
-        if (event.getType().equals("DIAL_IN")) {
-          totalUsers++;
-          if (!waitingQueue.offer(totalUsers)) {
-            System.out.println("Queue is full. Dropping call from user " + totalUsers);
-          }
-        } else if (event.getType().equals("HANG_UP")) {
-          numModems++;
+        Random rand = new Random();
+        int currentTime = 0;
+        int totalWaitTime = 0;
+        int totalModemBusyTime = 0;
+        int totalUsers = 0;
+
+        // Running the simulation for the specified length of time.
+        while (currentTime < lengthOfSimulation) {
+            // Randomly determining how many users are trying to dial in at the current time.
+            double lambda = averageDialInTime;
+            double p = Math.exp(-lambda);
+            int k = 0;
+            for (double product = rand.nextDouble(); product >= p; product *= rand.nextDouble()) {
+                k++;
+            }
+
+            // Creating dial-in events for the users.
+            for (int j = 0; j < k; j++) {
+                eventQueue.offer(new Event(nextUserId++, "DIAL_IN", currentTime, currentTime));
+                totalUsers++;
+            }
+
+            // Processing events that occur at the current time.
+            Event event = eventQueue.poll();
+            while (event != null && event.getTime() <= currentTime) {
+                if (event.getType().equals("DIAL_IN")) {
+                    // If the user dials in, we either assign a modem or place the user in the waiting queue.
+                    if (!waitingQueue.offer(event.getId())) {
+                        System.out.println("Queue is full. Dropping call from user " + event.getId());
+                    }
+                } else if (event.getType().equals("HANG_UP")) {
+                    // If the user hangs up, we free up a modem.
+                    numModems++;
+                }
+                event = eventQueue.poll();
+            }
+
+            if (event != null) {
+                eventQueue.offer(event);
+            }
+
+            // If we have free modems, assign them to users in the waiting queue.
+            while (numModems > 0 && waitingQueue.peek() != null) {
+                int userId = waitingQueue.poll();
+                totalWaitTime += (currentTime - userId);
+                int connectionTime = rand.nextInt(averageConnectionTime) + 1;
+                // Creating a hang-up event for the user after the connection time ends.
+                eventQueue.offer(new Event(userId, "HANG_UP", currentTime + connectionTime, 0));
+                totalModemBusyTime += connectionTime;
+                numModems--;
+            }
+
+            currentTime++;
         }
-        event = eventQueue.poll();
-      }
 
-      while (numModems > 0 && waitingQueue.peek() != null) {
-        int userId = waitingQueue.poll();
-        totalWaitTime += (currentTime - userId);
-        int connectionTime = rand.nextInt(averageConnectionTime) + 1;
-        eventQueue.offer(new Event(currentTime + connectionTime, "HANG_UP", currentTime + connectionTime));
-        totalModemBusyTime += connectionTime;
-        numModems--;
-      }
+        // Calculating and displaying results of the simulation.
+        double averageWaitTime = (totalUsers > 0) ? (double) totalWaitTime / totalUsers : 0;
+        double modemUsagePercentage = (numModems > 0) ? (double) totalModemBusyTime / (numModems * lengthOfSimulation) * 100 : 0;
 
-      currentTime++;
+        System.out.println("Average wait time: " + averageWaitTime);
+        System.out.println("Modem usage percentage: " + modemUsagePercentage);
+        System.out.println("Remaining users in queue: " + waitingQueue.size());
+
+        // Writing the results to report.txt.
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("report.txt", true))) {
+            File f = new File("report.txt");
+            if (f.length() == 0) {
+                writer.write("  1- Length of simulation\n  2- Average time between dial-in attempts\n   3- Average connection time\n   4- Number of modems in the bank\n  5- Size of the waiting queue, -1 for an infinite queue\n  6- Averge wait time\n   7- Percentage of time modems were busy\n  8- Number of customers left in the waiting queue\n ");
+                writer.write("    1   |    2   |    3   |    4   |    5   |     6     |     7     |    8\n");
+                writer.write("-----------------------------------------------------------------------------\n");
+            }
+            writer.write(String.format("%5d   | %5.1f  | %5.1f  | %5d  | %5d  | %9.2f  | %9.2f  | %5d\n",
+                lengthOfSimulation, averageDialInTime, (double) averageConnectionTime, numModems, queueCapacity,
+                averageWaitTime, modemUsagePercentage, waitingQueue.size()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
-    double averageWaitTime = (double) totalWaitTime / totalUsers;
-    double modemUsagePercentage = (double) totalModemBusyTime / (numModems * lengthOfSimulation) * 100;
-
-    System.out.println("Average wait time: " + averageWaitTime);
-    System.out.println("Modem usage percentage: " + modemUsagePercentage);
-    System.out.println("Remaining users in queue: " + waitingQueue.size());
-
-    try (BufferedWriter writer = new BufferedWriter(new FileWriter("report.txt", true))) {
-      File f = new File("report.txt");
-      if (f.length() == 0) {
-        writer.write("  1- Length of simulation\n  2- Average time between dial-in attempts\n   3- Average connection time\n   4- Number of modems in the bank\n  5- Size of the waiting queue, -1 for an infinite queue\n  6- Averge wait time\n   7- Percentage of time modems were busy\n  8- Number of customers left in the waiting queue\n ");
-        writer.write("    1   |    2   |    3   |    4   |    5   |     6     |     7     |    8\n");
-        writer.write("-----------------------------------------------------------------------------\n");
-      }
-      writer.write(String.format("%5d   | %5.1f  | %5.1f  | %5d  | %5d  | %9.2f  | %9.2f  | %5d\n",
-          lengthOfSimulation, averageDialInTime, (double) averageConnectionTime, numModems, queueCapacity,
-          averageWaitTime, modemUsagePercentage, waitingQueue.size()));
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    System.out.println("Simulation is complete: Summary in file report.txt");
-
-  }
 }
