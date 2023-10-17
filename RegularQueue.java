b6a3065973b0ffdf0c3938255ab0db45057efcd0
Talha Husnain <24100245@lums.edu.pk>
2023-10-17 12:30:42 -0700

    updated

--- RegularQueue.java
+++ RegularQueue.java
@@ -1,63 +1,122 @@
+/**
+ * A custom implementation of a queue that supports both static and dynamic
+ * (resizable) storage capacities.
+ * 
+ * @param <T> the type of elements held in this queue
+ */
 public class RegularQueue<T> {
-    private Object[] array;
-    private int front;
-    private int rear;
-    private int capacity;
-    private int size;
-    private boolean isDynamic;
 
-    public int size() {
-        return this.size;
-    }
+  // Array to store the elements of the queue.
+  private Object[] array;
 
-    public RegularQueue(int capacity, boolean isDynamic) {
-        this.capacity = capacity;
-        this.array = new Object[capacity];
-        this.front = 0;
-        this.rear = 0;
-        this.size = 0;
-        this.isDynamic = isDynamic;
-    }
+  // Index for the front of the queue.
+  private int front;
 
-    public boolean offer(T element) {
-        if (size == capacity) {
-            if (isDynamic) {
-                resize();
-            } else {
-                return false;
-            }
-        }
-        array[rear] = element;
-        rear = (rear + 1) % capacity;
-        size++;
-        return true;
-    }
+  // Index for the rear of the queue.
+  private int rear;
 
-    private void resize() {
-        int newCapacity = 2 * capacity;
-        Object[] newArray = new Object[newCapacity];
-        for (int i = 0; i < size; i++) {
-            newArray[i] = array[(front + i) % capacity];
-        }
-        array = newArray;
-        front = 0;
-        rear = size;
-        capacity = newCapacity;
-    }
+  // Maximum number of elements the queue can hold.
+  private int capacity;
 
-    public T poll() {
-        if (size == 0) return null;
-        T data = (T) array[front];
-        front = (front + 1) % capacity;
-        size--;
-        return data;
-    }
+  // Current number of elements in the queue.
+  private int size;
 
-    public T peek() {
-        return (size == 0) ? null : (T) array[front];
-    }
+  // Flag to indicate whether the queue should resize itself when full.
+  private boolean isDynamic;
 
-    public void clear() {
-        front = rear = size = 0;
+  /**
+   * Returns the current number of elements in the queue.
+   * 
+   * @return the size of the queue
+   */
+  public int size() {
+    return this.size;
+  }
+
+  /**
+   * Constructor to initialize the queue with a given capacity and resize
+   * behavior.
+   * 
+   * @param capacity  Initial capacity of the queue
+   * @param isDynamic If true, the queue will resize itself when full; otherwise,
+   *                  it will reject new elements.
+   */
+  public RegularQueue(int capacity, boolean isDynamic) {
+    this.capacity = capacity;
+    this.array = new Object[capacity];
+    this.front = 0;
+    this.rear = 0;
+    this.size = 0;
+    this.isDynamic = isDynamic;
+  }
+
+  /**
+   * Adds an element to the rear of the queue.
+   * If the queue is full and dynamic, it resizes itself. Otherwise, it rejects
+   * the new element.
+   * 
+   * @param element Element to be added
+   * @return true if the element was added, false otherwise
+   */
+  public boolean offer(T element) {
+    if (size == capacity) {
+      if (isDynamic) {
+        resize();
+      } else {
+        return false;
+      }
     }
+    array[rear] = element;
+    rear = (rear + 1) % capacity;
+    size++;
+    return true;
+  }
+
+  /**
+   * Doubles the capacity of the queue.
+   * Used when the queue is dynamic and is currently full.
+   */
+  private void resize() {
+    int newCapacity = 2 * capacity;
+    Object[] newArray = new Object[newCapacity];
+    for (int i = 0; i < size; i++) {
+      newArray[i] = array[(front + i) % capacity];
+    }
+    array = newArray;
+    front = 0;
+    rear = size;
+    capacity = newCapacity;
+  }
+
+  /**
+   * Removes and returns the front element of the queue.
+   * Returns null if the queue is empty.
+   * 
+   * @return the front element, or null if the queue is empty
+   */
+  public T poll() {
+    if (size == 0)
+      return null;
+    T data = (T) array[front];
+    front = (front + 1) % capacity;
+    size--;
+    return data;
+  }
+
+  /**
+   * Returns the front element of the queue without removing it.
+   * Returns null if the queue is empty.
+   * 
+   * @return the front element, or null if the queue is empty
+   */
+  public T peek() {
+    return (size == 0) ? null : (T) array[front];
+  }
+
+  /**
+   * Removes all elements from the queue.
+   */
+  public void clear() {
+    front = rear = size = 0;
+  }
 }
